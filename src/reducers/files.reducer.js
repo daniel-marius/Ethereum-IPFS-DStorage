0b64e64d68a8de233b1e345cfe2889e2067ec764
danielmarius1995 <50268474+danielmarius1995@users.noreply.github.com>
2021-02-11 23:08:05 +0200

    Update files.reducer.js

--- src/reducers/files.reducer.js
+++ src/reducers/files.reducer.js
@@ -5,10 +5,10 @@ import { BLOCKCHAIN_ITEMS, ETHEREUM_ERROR } from "../actions";
 const filesReducer = (state = [], action) => {
   switch (action.type) {
     case BLOCKCHAIN_ITEMS:
-      return action.payload;
+      return action.payload || [];
     // return { ...state, ..._.mapKeys(action.payload, "id") };
     case ETHEREUM_ERROR:
-      return action.payload;
+      return action.payload || ETHEREUM_ERROR;
     default:
       return state;
   }
